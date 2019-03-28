7873bb65a8f158666a34eb28fc4995bd4001b3fb
TruthSeekerBeast <truthseekerbeast@gmail.com>
2019-03-28 16:04:51 +0530

    Initial commit with running example

---
+++ dvs_running/poc.py
@@ -0,0 +1,23 @@
+import time
+start_time = time.time()
+import nbformat
+from nbparameterise import (
+    extract_parameters, replace_definitions, parameter_values
+)
+
+with open("DVS_ANALYSIS.ipynb") as f:
+    nb = nbformat.read(f, as_version=4)
+
+orig_parameters = extract_parameters(nb)
+params = parameter_values(orig_parameters, **{'PLOT_KIND': "box"})
+
+new_nb = replace_definitions(nb, params, execute=False)
+
+exec(new_nb.cells[0].source)
+exec(new_nb.cells[2].source)
+exec(new_nb.cells[5].source)
+
+a = ''
+exec('a = ' + new_nb.cells[6].source)
+a.savefig('abcd')
+print("--- %s seconds ---" % (time.time() - start_time))
