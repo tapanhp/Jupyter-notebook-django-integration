475bda5156e2f8be6a2e68b59d9bf1a5eb8697e3
TruthSeekerBeast <truthseekerbeast@gmail.com>
2019-04-01 10:51:05 +0530

    Full notebook run and test

--- dvs_running/Processor.py
+++ dvs_running/Processor.py
@@ -5,15 +5,20 @@ from nbparameterise import (
 from nbconvert.preprocessors import ExecutePreprocessor
 import time
 
+# To record time
 start_time = time.time()
 
+# As we are making changes directly to source file, pass it's path only
 SOURCE_IPYNB_FILE = "DVS_ANALYSIS.ipynb"
+TARGET_IPYNB_FILE = "COPIED_ANALYSIS.ipynb"
+
 
 with open(SOURCE_IPYNB_FILE) as f:
     nb = nbformat.read(f, as_version=4)
 
 orig_parameters = extract_parameters(nb)
 
+# Parameters to be passed
 param_dictionary = {'PLOT_HIST_SIZE': 30,
                     'PLOT_KIND': 'box',
                     'SCATTER_PLOT_LIMIT': 50,
@@ -27,7 +32,7 @@ new_nb = replace_definitions(nb, params, execute=False)
 ep = ExecutePreprocessor(timeout=2000, kernel_name='python3')
 ep.preprocess(new_nb, resources={'metadata': {'path': ''}})
 
-with open(SOURCE_IPYNB_FILE, 'wt') as f:
+with open(TARGET_IPYNB_FILE, 'wt') as f:
     nbformat.write(new_nb, f)
 
 print("\n--- %s seconds ---" % (time.time() - start_time))
