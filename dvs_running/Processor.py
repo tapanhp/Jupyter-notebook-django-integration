29e374ddf0f63a184fdfa85df85cd61de0418788
TruthSeekerBeast <truthseekerbeast@gmail.com>
2019-04-01 15:04:46 +0530

    Using nbimport to import notebooks as python modules and calling functions from within python script nbparameterised and nbconverter is no more needed, images are sent from notebook as bas64 encoded strings.

--- dvs_running/Processor.py
+++ dvs_running/Processor.py
@@ -1,3 +1,5 @@
+import pickle
+
 import nbformat
 from nbparameterise import (
     extract_parameters, replace_definitions, parameter_values
@@ -9,30 +11,36 @@ import time
 start_time = time.time()
 
 # As we are making changes directly to source file, pass it's path only
-SOURCE_IPYNB_FILE = "DVS_ANALYSIS.ipynb"
+SOURCE_IPYNB_FILE = "DVS_ANALYSIS_PROCEDURAL.ipynb"
 TARGET_IPYNB_FILE = "COPIED_ANALYSIS.ipynb"
 
 
 with open(SOURCE_IPYNB_FILE) as f:
-    nb = nbformat.read(f, as_version=4)
+    nb = nbformat.read(f, as_version=nbformat.NO_CONVERT)
 
 orig_parameters = extract_parameters(nb)
 
 # Parameters to be passed
 param_dictionary = {'PLOT_HIST_SIZE': 30,
-                    'PLOT_KIND': 'box',
+                    'PLOT_KIND': 'bar',
                     'SCATTER_PLOT_LIMIT': 50,
                     'BUBBLE_SIZE': 300.0,
                     'RANK': 30}
 
 params = parameter_values(orig_parameters, **param_dictionary)
 
-new_nb = replace_definitions(nb, params, execute=False)
-
-ep = ExecutePreprocessor(timeout=2000, kernel_name='python3')
-ep.preprocess(new_nb, resources={'metadata': {'path': ''}})
+new_nb = replace_definitions(nb, params, execute=True)
+print(new_nb.cells[4].outputs)
+# ep = ExecutePreprocessor(timeout=2000, kernel_name='python3')
+# ep.preprocess(new_nb, resources={'metadata': {'path': ''}})
 
 with open(TARGET_IPYNB_FILE, 'wt') as f:
     nbformat.write(new_nb, f)
+
+# with open(TARGET_IPYNB_FILE) as f:
+#     nb = nbformat.read(f, as_version=4)
+#     print(nb.cells[3].outputs[0].data['text/plain'])
+#     figure = nb.cells[3].outputs[0].data['text/plain']
+#     print(type(figure))
 
 print("\n--- %s seconds ---" % (time.time() - start_time))
---
+++ dvs_running/import_ipynb.py
@@ -0,0 +1,4 @@
+import nbimporter
+from DVS_ANALYSIS_FUNCTIONAL import bar_box
+
+print(bar_box('bar')[2:-1])
