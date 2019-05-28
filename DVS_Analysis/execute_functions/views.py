b63c24ecb215290788aa34c6eff1d6f598a306d5
TruthSeekerBeast <truthseekerbeast@gmail.com>
2019-05-28 17:28:56 +0530

    dynamically importing notebooks

--- DVS_Analysis/execute_functions/views.py
+++ DVS_Analysis/execute_functions/views.py
@@ -2,35 +2,46 @@ import json
 
 from django.http import HttpResponse
 from django.shortcuts import render
-import nbimporter
-from notebooks import DVS_ANALYSIS_FUNCTIONAL, wind_rose
-from plotly.offline.offline import _plot_html
+# from notebooks import DVS_ANALYSIS_FUNCTIONAL, wind_rose
+import importlib
+
+
+# from plotly.offline.offline import _plot_html
+
+
+def import_nb(notebook):
+    global i
+    import nbimporter
+    package = "notebooks"
+    name = notebook
+    i = getattr(__import__(package, fromlist=[name]), name)
 
 
 def execute(request):
+    import_nb("DVS_ANALYSIS_FUNCTIONAL")
     if request.method == 'POST':
         plot_kind = request.POST.get('plot_kind')
         if plot_kind:
-            bar_box = DVS_ANALYSIS_FUNCTIONAL.bar_box(plot_kind)
+            bar_box = i.bar_box(plot_kind)
         else:
-            bar_box = DVS_ANALYSIS_FUNCTIONAL.bar_box()
+            bar_box = i.bar_box()
 
         scatter_plot_limit = request.POST.get('scatter_plot_limit')
         bubble_size = request.POST.get('bubble_size')
         if scatter_plot_limit and bubble_size:
-            scatter = DVS_ANALYSIS_FUNCTIONAL.drawScatter(int(scatter_plot_limit), float(bubble_size))
+            scatter = i.drawScatter(int(scatter_plot_limit), float(bubble_size))
         elif scatter_plot_limit:
-            scatter = DVS_ANALYSIS_FUNCTIONAL.drawScatter(SCATTER_PLOT_LIMIT=int(scatter_plot_limit))
+            scatter = i.drawScatter(SCATTER_PLOT_LIMIT=int(scatter_plot_limit))
         elif bubble_size:
-            scatter = DVS_ANALYSIS_FUNCTIONAL.drawScatter(BUBBLE_SIZE=float(bubble_size))
+            scatter = i.drawScatter(BUBBLE_SIZE=float(bubble_size))
         else:
-            scatter = DVS_ANALYSIS_FUNCTIONAL.drawScatter()
+            scatter = i.drawScatter()
 
         rand_state = request.POST.get('rand_state')
         if rand_state:
-            cube = DVS_ANALYSIS_FUNCTIONAL.cube_helix(int(rand_state))
+            cube = i.cube_helix(int(rand_state))
         else:
-            cube = DVS_ANALYSIS_FUNCTIONAL.cube_helix()
+            cube = i.cube_helix()
         context = {
             'bar_box': bar_box[2:-1],
             'scatter': scatter[2:-1],
@@ -38,9 +49,9 @@ def execute(request):
         }
         return render(request, 'execute_functions/home.html', context)
     else:
-        bar_box = DVS_ANALYSIS_FUNCTIONAL.bar_box()
-        scatter = DVS_ANALYSIS_FUNCTIONAL.drawScatter()
-        cube = DVS_ANALYSIS_FUNCTIONAL.cube_helix()
+        bar_box = i.bar_box()
+        scatter = i.drawScatter()
+        cube = i.cube_helix()
         context = {
             'bar_box': bar_box[2:-1],
             'scatter': scatter[2:-1],
@@ -50,12 +61,12 @@ def execute(request):
 
 
 def draw(request):
-    interactive = wind_rose.draw_interactive()
-    plot_html, plotdivid, width, height = _plot_html(
-        interactive, None, True, '100%', 525, True, True)
+    interactive = j.draw_interactive()
+    # plot_html, plotdivid, width, height = _plot_html(
+    #     interactive, None, True, '100%', 525, True, True)
     response_data = {}
     response_data['result'] = 'success'
-    response_data['graph'] = plot_html
+    # response_data['graph'] = plot_html
     # response_data = json.dumps(response_data)
     # print(json.loads(response_data))
     return HttpResponse(json.dumps(response_data), content_type='application/json')
